c7c09150af16f7b5152f908a9931c1ded6452284
TabulateJarl8 <tabulatejarl8@gmail.com>
2023-10-19 16:40:03 -0400

    make Lbl struct better

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,21 +1,44 @@
 use crate::errors;
 
+/// Represents a label in the TI-BASIC bytecode format.
 #[derive(Debug, Clone, Copy)]
 pub struct Lbl {
+    /// The NULL byte padded hex name of the label
     pub name: [u8; 2],
-    pub size: u8,
-    pub memory_position: usize,
+    /// The memory address of the instruction after the '\n' after the Lbl name
+    /// WARNING: this can be out of bounds of the array length, so we need to check when
+    /// accessing it
+    pub skip_to_memory_position: usize,
 }
 
+/// The TI-BASIC bytecode interpreter. Hold information such the instruction stack, Lbl positions, etc.
 #[derive(Debug, Clone)]
 pub struct Interpreter {
+    /// The list of bytes for a given TI-BASIC program
     pub bytes: Vec<u8>,
+    /// A Vec of Lbl objects. Contains information on jumping to memory positions
     pub labels: Vec<Lbl>,
+    /// The pointer to the current address in the bytes memory
     pub bytes_pointer: usize,
 }
 
 impl Interpreter {
+    /// Creates a new interpreter with the given byte list.
+    ///
+    /// It will also locate and store the labels found in the bytecode.
+    ///
+    /// # Arguments
+    ///
+    /// * `bytes_list` - A vector of bytes representing the TI-BASIC bytecode.
+    ///
+    /// # Returns
+    ///
+    /// A new `Interpreter` instance.
+    ///
+    /// # Errors
+    /// Panics if there is an error in parsing the Lbl instructions.
     pub fn new(bytes_list: Vec<u8>) -> Self {
+        // TODO: dont panic
         let labels = find_labels(&bytes_list).unwrap();
         Self {
             bytes: bytes_list,
@@ -29,6 +52,15 @@ impl Interpreter {
     pub fn interpret_next_byte(&mut self) {}
 }
 
+/// Searches for labels within a list of bytes and returns them as a result.
+///
+/// # Arguments
+///
+/// * `bytes_list` - A reference to a vector of bytes representing the custom bytecode.
+///
+/// # Returns
+///
+/// A `Result` containing a vector of `Lbl` if labels are found, or an error if any issues occur.
 fn find_labels(bytes_list: &Vec<u8>) -> Result<Vec<Lbl>, anyhow::Error> {
     let lbl_addresses: Vec<usize> = bytes_list
         .iter()
@@ -80,11 +112,20 @@ fn find_labels(bytes_list: &Vec<u8>) -> Result<Vec<Lbl>, anyhow::Error> {
         };
 
         let label_name = [token1, token2];
+
+        // calculates the label name size. if it's only one byte, this will be calculated to 1, otherwise, 2
+        let size = 2 - (label_name[1] == 0) as usize;
+        // address is the address position of the Lbl text
+        // we then skip forward the length of the Lbl name + 2 to get to the next token
+        // adding 1 will get us to the '\n' on the same line as the Lbl, and adding 2 will get us
+        // to the first token of the next line
+        // WARNING: this can be out of bounds of the array length, so we need to check when
+        // accessing it
+        let next_instruction_address = address + size + 2;
+
         lbl_map.push(Lbl {
             name: label_name,
-            // calculates the label name size. if it's only one byte, this will be calculated to 1, otherwise, 2
-            size: 2 - (label_name[1] == 0) as u8,
-            memory_position: address,
+            skip_to_memory_position: next_instruction_address,
         });
     }
 
