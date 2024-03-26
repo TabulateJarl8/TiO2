90bbb86d0a13004b3f4a010fcd091c1324759ab9
Connor Sample <tabulatejarl8@gmail.com>
2024-03-26 01:13:53 -0400

    comments

--- src/interpreter/bytecode_parser.rs
+++ src/interpreter/bytecode_parser.rs
@@ -11,6 +11,7 @@ enum TIToken {
     Expression(Vec<u8>),
 }
 
+/// A struct representing the state of a TI program
 #[derive(Debug)]
 struct TIProgramState {
     tokens: Vec<TIToken>,
@@ -21,14 +22,17 @@ impl TIProgramState {
         Self { tokens: Vec::new() }
     }
 
+    /// Add a string token to the token list
     fn add_string(&mut self, token: String) {
         self.tokens.push(TIToken::String(token));
     }
 
+    /// Add a function token to the token list
     fn add_function(&mut self, token: u8) {
         self.tokens.push(TIToken::Function(token));
     }
 
+    /// Add a number token to the token list
     fn add_number(&mut self, token: f64) {
         self.tokens.push(TIToken::Number(token));
     }
@@ -45,11 +49,15 @@ pub fn tokenize_bytecode(ti_program: TIFile) {
         let current_token = bytecode[bytecode_pc];
 
         match current_token {
+            // check if the current token is `"` so that we can start tokenizing
+            // a string
             0x2A => {
                 // advance past the current " since we don't tokenize that
                 bytecode_pc += 1;
                 consume_string(&mut bytecode_pc, &bytecode, &mut program_state);
             }
+            // check if the current token is numerical, a `.`, or the negative sign
+            // if so, we parse a number
             0x30..=0x39 | 0x3A | 0xB0 => {
                 consume_number(&mut bytecode_pc, &bytecode, &mut program_state)
             }
@@ -61,12 +69,15 @@ pub fn tokenize_bytecode(ti_program: TIFile) {
     println!("{:x?}", program_state);
 }
 
+/// Try to consume a string from the current set of tokens
 fn consume_string(bytecode_pc: &mut usize, bytecode: &[u8], program_state: &mut TIProgramState) {
     let mut string_buffer = String::new();
     let mut consuming_string = true;
 
+    // loop until the string tokenization is complete
     while *bytecode_pc < bytecode.len() && consuming_string {
         let current_token = bytecode[*bytecode_pc];
+        // check if we're at the end of a string (`"` or `\n` is encountered)
         if current_token == 0x2A || current_token == 0x3F {
             consuming_string = false;
             program_state.add_string(string_buffer.clone());
@@ -74,24 +85,31 @@ fn consume_string(bytecode_pc: &mut usize, bytecode: &[u8], program_state: &mut 
             // we dont increment the pc here because its incremented in tokenize_bytecode
             // after the function returns
         } else {
+            // we're still in the string, so add it to the buffer and continue
             string_buffer += BYTE_TOKENS.get(&Byte::Single(current_token)).unwrap();
             *bytecode_pc += 1;
-            dbg!(&string_buffer);
         }
     }
 }
 
+/// Try to consume a number from the current set of tokens
 fn consume_number(bytecode_pc: &mut usize, bytecode: &[u8], program_state: &mut TIProgramState) {
     let mut consuming_number = true;
     let mut number_buffer = String::new();
 
+    // loop until the number tokenization is complete
     while *bytecode_pc < bytecode.len() && consuming_number {
         let current_token = bytecode[*bytecode_pc];
 
+        // check if the current token is still part of a number
         if (0x30..=0x39).contains(&current_token) || [0x3A, 0xB0].contains(&current_token) {
-            number_buffer += BYTE_TOKENS.get(&Byte::Single(current_token)).unwrap();
+            // we're still in a number so we can add it to the buffer and continue
+            number_buffer += BYTE_TOKENS
+                .get(&Byte::Single(current_token))
+                .expect("numerical tokens should be available in token map");
             *bytecode_pc += 1;
         } else {
+            // we're done with the number, parse the string as a f64 and add it as a token
             consuming_number = false;
             program_state.add_number(
                 number_buffer
